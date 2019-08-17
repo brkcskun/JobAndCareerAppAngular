e383b8a43c700f81fd44e9eb56c391aac819dfca
cilercx <41168697+cilercx@users.noreply.github.com>
2019-08-17 12:02:18 +0300

    Test

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthService {
+  constructor(private httpClient: HttpClient) {}
+
+  baseUrl = "http://localhost:4004/Users/";
+
+  login(model: any) {
+    return this.httpClient.post(this.baseUrl + "Authenticate", model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem("token", user.token);
+        }
+      })
+    );
+  }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  registerToggle() {
+    this.registerMode = true;
+  }
+
+  values: any;
+
+  getValues() {
+    this.httpClient.get("http://localhost:4004/api/values").subscribe(
+      response => {
+        this.values = response;
+        console.log(this.values);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = false;
+  }
+}
